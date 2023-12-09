57d2e9b9ef52a8dbc38c4070b797787f3fe4436f
Vijay Singh <vijaysinghaashu@gmail.com>
2023-12-09 11:01:02 +0530

    initial commit

---
+++ src/main/java/com/excellence_spring_jpa_jpql_query/SpringJpaQueryJpqlApplication.java
@@ -0,0 +1,13 @@
+package com.excellence_spring_jpa_jpql_query;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaQueryJpqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJpaQueryJpqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/excellence_spring_jpa_jpql_query/SpringJpaQueryJpqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.excellence_spring_jpa_jpql_query;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJpaQueryJpqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
